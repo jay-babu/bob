7eeb0fecdb0a7142ef3eb2198e8ed72b582983ae
Roman A. Grigorovich <ragrigorov@mts.ru>
2026-05-19 14:06:22 +0300

    feat(psql, sqlite): removed unneccessary expr.Glue

--- dialect/sqlite/im/qm.go
+++ dialect/sqlite/im/qm.go
@@ -117,7 +117,12 @@ func Excluded(column string) dialect.Expression {
 
 // Excluded references a column from the EXCLUDED pseudo-table in ON CONFLICT DO UPDATE.
 func Excluded(column string) dialect.Expression {
-	return dialect.Expression{}.New(expr.Glue(expr.Raw("EXCLUDED."), expr.Quote(column)))
+	return dialect.Expression{}.New(
+		expr.Join{
+			Exprs: []bob.Expression{expr.Raw("EXCLUDED."), expr.Quote(column)},
+			Sep:   expr.NoSep,
+		},
+	)
 }
 
 func SetExcluded(cols ...string) bob.Mod[*clause.ConflictClause] {
